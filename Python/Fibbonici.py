97c774d526fd633e14ba34dc46f4f3db12d3487d
studentorganizationofind <44507712+studentorganizationofind@users.noreply.github.com>
2022-10-28 00:12:42 +0530

    Create Fibbonici.py

---
+++ Python/Fibbonici.py
@@ -0,0 +1,16 @@
+# Python3 Program to
+# find sum of square
+# of first n natural
+# numbers
+
+# Return the sum of
+# square of first n
+# natural numbers
+def squaresum(n) :
+	return (n * (n + 1) * (2 * n + 1)) // 6
+
+# Driven Program
+n = 4
+print(squaresum(n))
+
+#This code is contributed by Nikita Tiwari.															
